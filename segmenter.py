2a7d437faf9dc16cd118914506a15edea96d3d3c
Mepuka Kessy <kessy002@umn.edu>
2013-07-13 15:36:05 -0500

    improved sentence segmenter

--- segmenter.py
+++ segmenter.py
@@ -1,5 +1,5 @@
 #Segmenter for word explore, contains algorithm and testing suites
-
+import pprint
 import nltk
 import simplejson as json
 
@@ -27,11 +27,33 @@ def extract_sent(text):
     #point for a new sentence, this list will likely grow as we train
     #the tokenizer are more text
 
-    abbrevs = ['cf', 'e.g', 'Sgt', 'sgt']
+    abbrevs = [
+               'cf',
+               'e.g',
+               'Sgt',
+               'sgt',
+               'etc',
+               'adv',
+               'n',
+               'v',
+               'adj',
+               'pron',
+               '+',
+               ]
+    #the text contains many abbreviations for centuries e.g. '12c.' or '14c.'
+    #so these must be exempted for causing sentence breaks
+    abbrevs = abbrevs + [str(date)+'c' for date in range(1,20)]
     for abbrev in abbrevs:
         punkt_tokenizer._params.abbrev_types.add(abbrev)
 
+    return punkt_tokenizer.sentences_from_text(text, realign_boundaries=True)
 
+def print_test(test_sents):
+
+    for word, tokenized_sent in test_sents:
+        print 'Word:\t%s\n\n' % (word)
+        pprint.pprint(tokenized_sent)
+        print '\n===================================\n'
 
 def seg_test():
     """Tests the quality of the segmenter based of a predefined
@@ -57,10 +79,9 @@ def seg_test():
                   'cap (n.)',
                  ]
 
-    test_sents = [words[test_word] for test_word in test_words]
-
-
-
+    test_sents = [(test_word, words[test_word].split('\n')[0]) for test_word in test_words]
+    test_sents = [(test_word, list(extract_sent(sent))) for test_word, sent in test_sents]
+    print_test(test_sents)
 
 if __name__ == '__main__':
     seg_test()
